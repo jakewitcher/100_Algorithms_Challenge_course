94d1656cba5152a004a161a50dfa443ffbc24d4a
Jake Witcher <jjwitcher09@gmail.com>
2018-11-24 00:31:43 -0500

    solution to adding digits algorithm

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
-function addTwoDigits(n: any): number {
-
+function addDigits(num: number): number {
+    const numArray: string[] = num.toString().split("");
+    return numArray.map((x) => Number(x)).reduce((a, b) => a + b);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addDigits(29)); // 11
+console.log(addDigits(46)); // 10
+console.log(addDigits(786)); // 21
+console.log(addDigits(786538)); // 37
