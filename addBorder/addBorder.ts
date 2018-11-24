6319864a84127a8db7caaa6c1a555d6c87056111
Jake Witcher <jjwitcher09@gmail.com>
2018-11-24 00:05:43 -0500

    solution to add border algorithm

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,11 @@
 function addBorder(picture: string[]): string[] {
-
+    const width: number = picture[0].length;
+    let frame: string = "";
+    for (let i = 0; i < width; i++) {
+        frame += "*";
+    }
+    picture = [frame, ...picture, frame];
+    return picture.map((x) => `*${x}*`);
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"])); // [ "*****", "*abc*", "*ded*", "*****"]
