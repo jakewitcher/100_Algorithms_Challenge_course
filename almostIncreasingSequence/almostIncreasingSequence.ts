585cde8a181c07e34c881e34755251a5728fbffd
Jake Witcher <jjwitcher09@gmail.com>
2018-11-25 16:52:07 -0500

    added alternate solution to almost increasing sequence algorithm, using forEach w/ index param

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -2,7 +2,7 @@ function almostIncreasingSequence(sequence: number[]): boolean {
     let count = 0;
     sequence.reduce((a: number, b: number) => {
         if (a >= b) {
-            count += 1;
+            count++;
             return b;
         }
         return b;
@@ -10,6 +10,22 @@ function almostIncreasingSequence(sequence: number[]): boolean {
     return count <= 1;
 }
 
+function almostIncreasingSequence2(sequence: number[]): boolean {
+    let count = 0;
+    sequence.forEach((n: number, i: number) => {
+        if (n >= sequence[i + 1]) {
+            count++;
+            return n;
+        }
+        return n;
+    });
+    return count <= 1;
+}
+
 console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
 console.log(almostIncreasingSequence([1, 3, 2])); // true
 console.log(almostIncreasingSequence([1, 2, 3, 4, 5, 6, 7, 4, 8, 9, 10, 15, 12])); // false
+
+console.log(almostIncreasingSequence2([1, 3, 2, 1])); // false
+console.log(almostIncreasingSequence2([1, 3, 2])); // true
+console.log(almostIncreasingSequence2([1, 2, 3, 4, 5, 6, 7, 4, 8, 9, 10, 15, 12])); // false
