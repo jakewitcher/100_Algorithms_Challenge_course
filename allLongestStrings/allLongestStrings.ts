cd92193ebdd1439b605bb2b2a89ffbe23531282a
Jake Witcher <jjwitcher09@gmail.com>
2018-11-25 15:54:00 -0500

    solution to all longest strings algorithm

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,14 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+    const longestString: string = inputArray.reduce((a, b) => {
+        if (a.length > b.length) {
+            return a;
+        }
+        return b;
+    });
+    return inputArray.filter((str) => str.length === longestString.length);
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"])); // [ "aba", "vcd", "aba"]
+console.log(allLongestStrings(["abafe", "aa", "adega", "vcd", "aba", "rolsd", "b", "abdrc"]));
+// ["abafe", "adega", "rolsd", "abdrc"]
+console.log(allLongestStrings(["aba", "aa", "adca", "vcd", "abad"])); // ["adca", "abad"]
