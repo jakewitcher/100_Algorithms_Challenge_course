857239e143f73a9b6bad845f9e5f04c7f050fe0d
Jake Witcher <jjwitcher09@gmail.com>
2018-11-25 16:32:07 -0500

    solution to almost increasing sequence algorithm

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,15 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let count = 0;
+    sequence.reduce((a: number, b: number) => {
+        if (a >= b) {
+            count += 1;
+            return b;
+        }
+        return b;
+    });
+    return count > 1 ? false : true;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])); // false
+console.log(almostIncreasingSequence([1, 3, 2])); // true
+console.log(almostIncreasingSequence([1, 2, 3, 4, 5, 6, 7, 4, 8, 9, 10, 15, 12])); // false
