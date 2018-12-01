acb7d0d3c83b66b3776fd0437c28b68660c291ad
Jake Witcher <jjwitcher09@gmail.com>
2018-11-30 20:58:58 -0500

    solution to alphabet subsequence algorithm

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,20 @@
 function alphabetSubsequence(s: string): boolean {
+    const charArray: string[] = s.split("");
+    const charCodeArray: number[] = charArray.map((char) => char.charCodeAt(0));
 
+    let count: number = 0;
+    charCodeArray.reduce((a, b) => {
+        if (a >= b) {
+            count++;
+            return b;
+        }
+        return b;
+    });
+    return count === 0;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab")); // false
+console.log(alphabetSubsequence("effg")); // false
+console.log(alphabetSubsequence("cdce")); // false
+console.log(alphabetSubsequence("ace")); // true
+console.log(alphabetSubsequence("bxz")); // true
