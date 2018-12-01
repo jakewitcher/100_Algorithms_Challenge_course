7bd1e2550500728a450f18c9265278778ee21d19
Jake Witcher <jjwitcher09@gmail.com>
2018-11-30 21:11:54 -0500

    add second solution to alphabetic shift algorithm using character codes

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,4 +1,4 @@
-function alphabeticShift(inputString: string): string {
+function alphabeticShift1(inputString: string): string {
     const alphabet: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
         "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -13,5 +13,20 @@ function alphabeticShift(inputString: string): string {
     return shift.reduce((a, b) => a + b, "");
 }
 
-console.log(alphabeticShift("crazy")); // dsbaz
-console.log(alphabeticShift("labradoodle")); // mbcsbeppemf
+function alphabeticShift2(inputString: string): string {
+    const wordArray: string[] = inputString.split("");
+    const charCodeArray: number[] = wordArray.map((char) => {
+        if (char === "z") {
+            char = "a";
+            return char.charCodeAt(0);
+        }
+        return char.charCodeAt(0) + 1;
+    });
+    return charCodeArray.map((c) => String.fromCharCode(c)).reduce((a, b) => a + b);
+}
+
+console.log(alphabeticShift1("crazy")); // dsbaz
+console.log(alphabeticShift1("labradoodle")); // mbcsbeppemf
+
+console.log(alphabeticShift2("crazy")); // dsbaz
+console.log(alphabeticShift2("labradoodle")); // mbcsbeppemf
