23afa2a1cfcac136ebbf7632dc6abdf9e51d65b5
Jake Witcher <jjwitcher09@gmail.com>
2018-12-04 23:01:50 -0500

    add two solutions for alternating sums algorithm

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,26 @@
-function alternatingSums(a: number[]): number[] {
+// solution using filter and reduce
 
+function alternatingSums(a: number[]): number[] {
+    const oddIndexSums: number = a.filter((num, index) => index % 2 === 0).reduce((x, y) => x + y);
+    const evenIndexSums: number = a.filter((num, index) => index % 2 !== 0).reduce((x, y) => x + y);
+    return [oddIndexSums, evenIndexSums];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70])); // [180, 105]
+
+// solution using for loop
+
+function alternatingSums2(a: number[]): number[] {
+    let odd: number = 0;
+    let even: number = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (i % 2 === 0) {
+            odd += a[i];
+        } else {
+            even += a[i];
+        }
+    }
+    return [odd, even];
+}
+
+console.log(alternatingSums2([50, 60, 60, 45, 70])); // [180, 105]
