6301b037e2657a0b0923969407839642be9a3458
Jake Witcher <jjwitcher09@gmail.com>
2018-12-06 12:20:56 -0500

    add alt solutions to adjacent elements product

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -11,3 +11,41 @@ console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); // 21
 console.log(adjacentElementsProduct([23, 18, -2, -25, 17, 13])); // 414
 console.log(adjacentElementsProduct([6, -12, -5, 2, 3])); // 60
 console.log(adjacentElementsProduct([61, 7, -21, -5, 17, 32, 12, -6, 81, 4])); // 544
+
+function adjacentElementsProduct2(inputArray: number[]): number {
+    const maxProduct: number = inputArray.map((num: number, index: number) => {
+        if (index < inputArray.length - 1) {
+            return num * inputArray[index + 1];
+        }
+    }).reduce((a, b) => {
+        if (b === undefined || a >= b) {
+            return a;
+        }
+        return b;
+    });
+    return maxProduct;
+}
+
+console.log(adjacentElementsProduct2([3, 6, -2, -5, 7, 3])); // 21
+console.log(adjacentElementsProduct2([23, 18, -2, -25, 17, 13])); // 414
+console.log(adjacentElementsProduct2([6, -12, -5, 2, 3])); // 60
+console.log(adjacentElementsProduct2([61, 7, -21, -5, 17, 32, 12, -6, 81, 4])); // 544
+
+function adjacentElementsProduct3(inputArray: number[]): number {
+    const maxProduct: number = inputArray.reduce((a, b, i) => {
+        if (inputArray[i + 1] === undefined) {
+            return a;
+        } else if (i === 1) {
+            return a * b;
+        } else if (a >= b * inputArray[i + 1]) {
+            return a;
+        }
+        return b * inputArray[i + 1];
+    });
+    return maxProduct;
+}
+
+console.log(adjacentElementsProduct3([3, 6, -2, -5, 7, 3])); // 21
+console.log(adjacentElementsProduct3([23, 18, -2, -25, 17, 13])); // 414
+console.log(adjacentElementsProduct3([6, -12, -5, 2, 3])); // 60
+console.log(adjacentElementsProduct3([61, 7, -21, -5, 17, 32, 12, -6, 81, 4])); // 544
