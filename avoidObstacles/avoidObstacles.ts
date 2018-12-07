13dd44c8156277251ade24f2ce97e43cd515cfec
Jake Witcher <jjwitcher09@gmail.com>
2018-12-06 20:47:00 -0500

    add solution to avoid obstacles algorithm

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,10 @@
-function avoidObstacles(inputArray: number[]): number {
-
+function avoidObstacles(jump: number, inputArray: number[]): number {
+    if (inputArray.filter((num) => num % jump === 0).length === 0) {
+        return jump;
+    }
+    return avoidObstacles(jump + 1, inputArray);
 }
 
-console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles(2, [5, 3, 6, 7, 9])); // 4
+console.log(avoidObstacles(2, [7, 2, 9, 8, 10])); // 6
+console.log(avoidObstacles(2, [1, 4, 5, 12, 7])); // 8
