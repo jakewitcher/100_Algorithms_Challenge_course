3fba61b43f9b0e75202322288ed9a3f899062936
Jake Witcher <jjwitcher09@gmail.com>
2018-12-06 20:02:14 -0500

    add solution to array replace algorithm

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,12 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+    return inputArray.map((num) => {
+        if (num === elemToReplace) {
+            return substitutionElem;
+        }
+        return num;
+    });
 }
 
-console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3)); // [3, 2, 3]
+console.log(arrayReplace([4, 5, 2, 3, 5, 6], 5, 0)); // [4, 0, 2, 3, 0, 6]
+console.log(arrayReplace([7, 9, 7, 5, 4, 7, 3, 7], 7, -4)); // [-4, 9, -4, 5, 4, -4, 3, -4]
